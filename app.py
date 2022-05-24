370fda5a9329c95ed5758cb14e9e987db40dd149
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-05-24 14:01:08 -0500

    complete madlibs exercise

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-
+from stories import story
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -9,6 +9,14 @@ app.config['SECRET_KEY'] = "chickenzarecool21837"
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
-def home_page():
+def home_page(): 
     """Shows home page"""
-    return render_template('home.html')
+    words = story.prompts
+    return render_template('home.html', words = words)
+
+
+@app.route('/story')
+def story_page():
+    user_words = request.args
+    text = story.generate(user_words)
+    return render_template('story.html',text = text)
